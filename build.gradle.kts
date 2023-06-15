701effc6831106148d0c41d21bc5dcd14dcac9bb
yair <yair@tencyle.com>
2023-06-15 14:57:19 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+
+plugins {
+    kotlin("jvm") version "1.6.0"
+}
+
+group = "com.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    implementation("com.fasterxml.jackson.core:jackson-databind:" + "2.12" + ".6")
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
